489aae9215e7606af6c262a6eb03fbe33a792380
Gabriel Viana <gabrielviana630@gmail.com>
2025-12-10 22:47:21 -0300

    registro do CRUD de Categorias no main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ from repo import (
     chamado_repo,
     chamado_interacao_repo,
     indices_repo,
+    categoria_repo
 )
 from repo import chat_sala_repo, chat_participante_repo, chat_mensagem_repo
 
@@ -43,6 +44,7 @@ from routes.usuario_routes import router as usuario_router
 from routes.chat_routes import router as chat_router
 from routes.public_routes import router as public_router
 from routes.examples_routes import router as examples_router
+from routes.admin_categorias_routes import router as admin_categorias_router
 
 # Seeds
 from util.seed_data import inicializar_dados
@@ -82,6 +84,7 @@ TABELAS = [
     (chat_sala_repo, "chat_sala"),
     (chat_participante_repo, "chat_participante"),
     (chat_mensagem_repo, "chat_mensagem"),
+    (categoria_repo, "categoria"),
 ]
 
 # Criar tabelas do banco de dados
@@ -125,6 +128,7 @@ ROUTERS = [
     (chat_router, ["Chat"], "chat"),
     (public_router, ["Público"], "público"),
     (examples_router, ["Exemplos"], "exemplos"),
+    (admin_categorias_router, ["Admin - Categorias"], "admin de categorias"),  # NOVO ROUTER
 ]
 
 # Incluir routers
